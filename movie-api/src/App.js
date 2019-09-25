89866ae4900989df2141243c664b227f39fe2973
josipoviciwan <josipoviciwan@gmail.com>
2019-09-25 09:49:22 +0200

    Authorization (Guest token)

--- movie-api/src/App.js
+++ movie-api/src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { MovieDetails, MainScreen } from "./components";
+
 function App() {
+  let fetchGuestToken = () => {
+    fetch(
+      "https://api.themoviedb.org/3/authentication/guest_session/new?api_key=f57efe1486f26a1000ecc7f73ebf0005"
+    )
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data => {
+        console.log("New guest token fetched")
+        sessionStorage.setItem("guestToken", JSON.stringify(data));
+      });
+  };
+
+  useEffect(() => {
+    let token = JSON.parse(sessionStorage.getItem("guestToken"));
+    if (token) {
+      let dateString = token.expires_at;
+      let expireDate = new Date(dateString);
+      let currDate = new Date();
+
+      if (expireDate < currDate) {
+        fetchGuestToken();
+      }
+    } else {
+      fetchGuestToken();
+    }
+  });
   return (
     <BrowserRouter>
       <main className="container">
